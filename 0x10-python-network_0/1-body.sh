ee5438da15aa89e866b5528943c2cbae57b59a97
daph41 <daphinemoraa@gmail.com>
2023-07-03 02:50:13 -0700

    python network

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# send a GET request to an URL with curl, and display the body of the response
-curl -sL "$1"
+# takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sX GET $1 -L
 
 
