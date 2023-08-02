8b7725f8aa2d96e661da00e43a9323537d436f7c
daph41 <daphinemoraa@gmail.com>
2023-08-02 04:03:06 -0700

    js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,15 @@
 #!/usr/bin/node
-
+// a script that reads and prints the content of a file
 const fs = require('fs');
-const file = process.argv[2];
+const process = require('process');
 
-fs.readFile(file, 'utf-8', function (err, data) {
+const args = process.argv.slice(2);
+
+fs.readFile(args[0], (err, data) => {
   if (err) {
     console.log(err);
   } else {
-    console.log(data);
+    console.log(data.toString());
   }
 });
+
